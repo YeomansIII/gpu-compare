2c727e292a9fbe1d2c8b0db39784139dfd6fac9b
Cody <ccraft2@gmu.edu>
2017-05-03 18:15:39 -0400

    Added the DatePicker and the Img Hover effect

--- IT431Site/App_Start/BundleConfig.cs
+++ IT431Site/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace IT431Site
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/ImgHover.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
